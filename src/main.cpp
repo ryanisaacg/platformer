4abd568524db5e51103607808c2b7706150a0859
EpicPieGuy <ryanisaacg@gmail.com>
2016-08-26 08:01:18 -0400

    Project now builds on Windows

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include "tilemap.h"
 #include "rect.h"
 
+#undef main
+
 int main() {
 	TileMap<rect> map(640, 480, 32);
 	SDL_Init(SDL_INIT_VIDEO);
@@ -9,5 +11,6 @@ int main() {
 	SDL_Delay(1000);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
+	return 0;
 }
 
