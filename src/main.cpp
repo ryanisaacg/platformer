762139bee4f075817f446ade98ff61405aa2fdf4
Ryan Goldstein <ryanisaacg@gmail.com>
2016-08-26 21:42:21 -0400

    Started to test the physics system

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,23 @@
+#include <iostream>
+
 #include "SDL.h"
-#include "tilemap.h"
+
+#include "physics.h"
 #include "rect.h"
+#include "tilemap.h"
+
 
 #undef main
 
 int main() {
-	TileMap<Rect> map(640, 480, 32);
+	TileMap<Entity> map(640, 480, 32);
+	map[Vector2(0, 0)] = Entity(0, 480, 32, 32, nullptr);
+	auto ent = Entity(0, 0, 32, 32, nullptr);
+	std::vector<Entity> entities;
+	entities.push_back(ent);
+	for(int i = 0; i < 30; i++)
+		update(map, entities);
+	std::cout << entities[0].y;
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_Window *window = SDL_CreateWindow("Platformer", 10, 10, 640, 480, SDL_WINDOW_SHOWN);
 	SDL_Delay(1000);
