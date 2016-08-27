469e5fced48a810bb677a6cd338edf4a37d1388e
Ryan Goldstein <ryanisaacg@gmail.com>
2016-08-27 10:12:21 -0400

    Fixed bug where the map had an entity put in the wrong place

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@
 
 int main() {
 	TileMap<Optional<Entity>> map(640, 480, 32);
-	map[Vector2(0, 0)] = Entity(0, 480, 32, 32, nullptr);
+	map[Vector2(0, 448)] = Entity(0, 480, 32, 32, nullptr);
 	auto ent = Entity(0, 0, 32, 32, nullptr);
 	std::vector<Entity> entities;
 	entities.push_back(ent);
