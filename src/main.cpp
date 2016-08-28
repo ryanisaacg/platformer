6fc8d743461abbb6c3ef2bf927bdac33bf59af40
Ryan Goldstein <ryanisaacg@gmail.com>
2016-08-28 16:12:39 -0400

    Set up separate-threaded updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <thread>
 #include <vector>
 
 #include "SDL.h"
@@ -14,19 +15,30 @@
 
 using namespace std;
 
+bool run_loop = true;
+
+TileMap<Optional<Entity>> map(640, 480, 32);
+vector<Entity> entities;
+
+void update_loop() {
+	while(run_loop) {
+		//STATE UPDATE
+		update(map, entities);
+		SDL_Delay(16);
+	}
+}
+
 #undef main
 int main() {
 	string title = "Platformer";
 	auto window = Window(title.c_str(), 640, 480);
-	TileMap<Optional<Entity>> map(640, 480, 32);
-	vector<Entity> entities;
 	Keyboard keyboard;
 	auto texture = window.load_texture("../img/red.png");
 	map[Vector2(0, 448)] = Entity(0, 448, 32, 32, texture);
 	auto ent = Entity(0, 0, 32, 32, texture);
 	entities.push_back(ent);
 	auto controller = PlayerController(keyboard, map, entities[0]);
-	bool run_loop = true;
+	auto update_thread = thread(update_loop);
 	while(run_loop) {
 		//EVENT POLLING
 		SDL_Event e;
@@ -34,9 +46,7 @@ int main() {
 			if(e.type == SDL_QUIT) run_loop = false;
 			else keyboard.process(e);
 		}
-		//STATE UPDATE
 		controller.update();
-		update(map, entities);
 		//RENDER CODE
 		window.start();
 		for(auto &ent : entities)
@@ -50,8 +60,9 @@ int main() {
 			}
 		}
 		window.end();
-		SDL_Delay(16);
+		SDL_Delay(1);
 	}
+	update_thread.join();
 	return 0;
 }
 
