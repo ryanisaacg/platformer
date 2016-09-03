cddf4aedf87cb8c3ab544da415366ccdddf3788b
EpicPieGuy <ryanisaacg@gmail.com>
2016-09-03 10:49:12 -0400

    Worked on integrating LeveData and State

--- src/level.cpp
+++ src/level.cpp
@@ -1,18 +1,31 @@
 #include "level.h"
 
-#include <ifstream>
-#include <ofstream>
+#include <fstream>
 #include <sstream>
 
-LevelData::LevelData(int width, int height, int square_size) : width(width), height(height), square_size(square_size), data() {}
+LevelData::LevelData(int width, int height, int square_size) : width(width), height(height), square_size(square_size), data() {
+	expand_to_capacity();
+}
+
 LevelData::LevelData(std::string filename, int width, int height, int square_size) : LevelData(width, height, square_size) {
 	load(filename);
+	expand_to_capacity();
+}
+
+void LevelData::expand_to_capacity() {
+	auto old = data;
+	data = std::vector<char>(width * height);
+	for(int i = 0; i < old.size(); i++)
+		data[i] = old[i];
+	for(int i = old.size(); i < data.size(); i++)
+		data[i] = '\0';
 }
 
 char& LevelData::operator[](Vector2 point) {
 	int x = (int)point.x / square_size;
 	int y = (int)point.y / square_size;
-	return data[y * width + x];
+	int index = y * width + x;
+	return data[index];
 }
 
 void LevelData::save(const std::string filename) const {
@@ -28,7 +41,54 @@ void LevelData::load(const std::string filename) {
 	buffer << t.rdbuf();
 	auto str = buffer.str();
 	int length = str.length();
-	data = std::vector(length);
+	data = std::vector<char>(length);
 	for(int i = 0; i < length; i++)
-		data[i] = str.char_at(i);
+		data[i] = str.at(i);
+}
+
+
+Level::Level(int width, int height, Window &window) : data(width, height, TILE_SIZE), 
+	state(width, height, TILE_SIZE), width(width), height(height) {
+	wall = window.load_texture("../img/red.png");
+}
+
+Level::Level(std::string filename, int width, int height, Window &window) : 
+	data(filename, width, height, TILE_SIZE), state(width, height, TILE_SIZE), width(width), height(height) {
+	wall = window.load_texture("../img/red.png");
+	restart();
+}
+
+void Level::place_tile(const Vector2 point) {
+	state.place_tile(point, wall);
+	data[point] = 'X';
+}
+
+void Level::update() {
+	state.update();
+}
+
+void Level::restart() {
+	for(int x = 0; x < width; x += TILE_SIZE) {
+		for(int y = 0; y < height; y += TILE_SIZE) {
+			auto pos = Vector2(x, y);
+			char square = data[pos];
+			switch(square) {
+			case ' ':
+				break;
+			case 'X':
+				state.place_tile(pos, wall);
+				break;
+			case 'P': {
+				auto &player = state.spawn(Rect(0, 0, 32, 32), wall, false, ControlType::PLAYER);
+				player.alignment = Alignment::PLAYER;
+				player.gravity = 0.5f;
+			} break;
+			case 'E': {
+				auto &enemy = state.spawn(Rect(200, 0, 32, 32), wall);
+				enemy.alignment = Alignment::ENEMY;
+				enemy.gravity = 0.5f;
+			} break;
+			}
+		}
+	}
 }
--- src/level.h
+++ src/level.h
@@ -4,7 +4,10 @@
 #include <string>
 #include <vector>
 
+#include "SDL.h"
+
 #include "state.h"
+#include "window.h"
 
 class LevelData {
 public:
@@ -14,14 +17,22 @@ public:
 	void save(const std::string filename) const;
 	void load(const std::string filename);
 private:
+	void expand_to_capacity();
 	std::vector<char> data;
 	const int width, height, square_size;
 };
 
 struct Level {
-	Level(int width, int height);
-	Level(std::string filename, int width, int height);
-	void place_tile(const Vector2 point, SDL_Texture *texture);
+public:
+	Level(int width, int height, Window &window);
+	Level(std::string filename, int width, int height, Window &window);
+	void place_tile(const Vector2 point);
+	void restart();
+	void update();
 	LevelData data;
 	State state;
+	static const int TILE_SIZE = 32;
+private:
+	SDL_Texture *wall;
+	int width, height;
 };
