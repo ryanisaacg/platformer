558b3fd2154dfc164b55767fe8950b0a40cce8e6
Ryan Goldstein <ryanisaacg@gmail.com>
2016-09-06 20:20:41 -0400

    Set up window scaling for the mouse position

--- src/mouse.cpp
+++ src/mouse.cpp
@@ -2,12 +2,18 @@
 
 #include "SDL.h"
 
-Mouse::Mouse() {
+Mouse::Mouse() : Mouse(1, 1) {
+	update();
+}
+
+Mouse::Mouse(float xscale, float yscale) : xscale(xscale), yscale(yscale) {
 	update();
 }
 
 void Mouse::update() {
 	auto buttonmask = SDL_GetMouseState(&x, &y);
+	x = (int)(x / xscale);
+	y = (int)(y / yscale);
 	left = buttonmask & SDL_BUTTON_LMASK;
 	right = buttonmask & SDL_BUTTON_RMASK;
 	middle = buttonmask & SDL_BUTTON_MMASK;
--- src/mouse.h
+++ src/mouse.h
@@ -2,7 +2,9 @@
 
 struct Mouse {
 	Mouse();
+	Mouse(float xscale, float yscale);
 	void update();
 	bool left, middle, right, x1, x2;
 	int x, y;
+	const float xscale, yscale;
 };
